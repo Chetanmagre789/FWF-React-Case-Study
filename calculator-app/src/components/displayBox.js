b4762c214467527d4da15aaed9f0fa6c13a9b77f
Chetanmagre789 <38548394+Chetanmagre789@users.noreply.github.com>
2018-06-22 19:08:06 +0530

    Delete displayBox.js

--- calculator-app/src/components/displayBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const DisplayBox =(props)=> {
-return(
-    <input type="text" readOnly value={props.expression} className="display"/>
-    )
-}
-
-export default DisplayBox;
